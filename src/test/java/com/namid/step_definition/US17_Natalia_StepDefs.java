96095467970623b5be0b68ba611afcc3859808e6
nkfl2022 <nkfl@yahoo.com>
2023-07-20 12:33:58 -0400

    saving changes in stepDefs and feature files US06 and US17

---
+++ src/test/java/com/namid/step_definition/US17_Natalia_StepDefs.java
@@ -0,0 +1,37 @@
+package com.namid.step_definition;
+
+import com.namid.pages.LoginPage;
+import com.namid.utilities.ConfigurationReader;
+import com.namid.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class US17_Natalia_StepDefs {
+
+    LoginPage loginPage = new LoginPage();
+    @Given("User logged in as Sales Manager")
+    public void user_logged_in_as_sales_manager() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        loginPage.loginInput.sendKeys("salesmanager15@info.com");
+        loginPage.passwordInput.sendKeys("salesmanager");
+        loginPage.loginBtn.click();
+
+    }
+    @Then("User should be able to access nineteen modules")
+    public void user_should_be_able_to_access_nineteen_modules() {
+
+        List<WebElement> allModules = Driver.getDriver().findElements(By.xpath("//li[@style='display: block;']//span"));
+
+        for (WebElement each : allModules){
+            Assert.assertTrue(each.isDisplayed());
+        }
+
+    }
+
+
+}
---
+++ src/test/resources/features/US17_Natalia.feature
@@ -0,0 +1,9 @@
+@B29G30-182
+Feature: US17 As a sales manager, I should be able to access the different modules.
+
+  Scenario: AC-1 Verify the sales managers access to 19 modules.
+
+    Given User logged in as Sales Manager
+    Then User should be able to access nineteen modules
+
+
