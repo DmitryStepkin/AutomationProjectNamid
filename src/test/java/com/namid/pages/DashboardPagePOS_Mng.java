679d3db8412e6b4d6ccabb67623c319175e4efa7
JelenaMack23 <mackovic.jelena@yahoo.com>
2023-07-20 12:15:54 -0400

    US13_Jelena

--- src/test/java/com/namid/pages/DashboardPagePOS_Mng.java
+++ src/test/java/com/namid/pages/DashboardPagePOS_Mng.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class DashboardPagePOS_Mng {
 
     public DashboardPagePOS_Mng(){
@@ -14,4 +16,8 @@ public class DashboardPagePOS_Mng {
 
     @FindBy (xpath = "//a[@data-menu='484']")
     public WebElement pointOfSale;
+
+    @FindBy (xpath = "//nav[@class='navbar navbar-inverse']")
+    public List<WebElement> allModules;
+
 }
